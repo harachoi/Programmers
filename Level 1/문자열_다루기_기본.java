cbd249cb2126b668fea8347ea0c6125bfa6d02d9
Hara Choi <hara77227722@gmail.com>
2021-05-09 21:08:14 +0900

    Create 문자열_다루기_기본.java

---
+++ Level 1/문자열_다루기_기본.java
@@ -0,0 +1,11 @@
+class Solution {
+  public boolean solution(String s) {
+      if(s.length() == 4 || s.length() == 6){
+          try{
+              Integer.parseInt(s);
+              return true;
+          } catch(NumberFormatException e) return false;
+      }
+      return false;
+  }
+}
